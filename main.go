b36b7ba0e710d9e37cd6a184a9cfdc70080523b1
unknown <syahrilatsar@gmail.com>
2026-02-02 19:51:33 +0700

    layered arch, env, database

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+type Config struct {
+	Port    string `mapstructure:"PORT"`
+}
+
 import (
 	"encoding/json"
 	"fmt"
@@ -163,4 +167,44 @@ func main() {
 	if err != nil {
 		fmt.Println("gagal running server")
 	}
+
 }
+
+viper.AutomaticEnv()
+viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+if _, err := os.Stat(".env"); err == nil {
+	viper.SetConfigFile(".env")
+	_ = viper.ReadInConfig()
+}
+config := Config{
+ 	Port: viper.GetString("PORT"),
+}
+addr := "0.0.0.0:" + config.Port
+fmt.Println("Server running di", addr)
+
+err = http.ListenAndServe(addr, nil)
+if err != nil {
+	fmt.Println("gagal running server", err)
+}
+// ubah Config
+type Config struct {
+	Port   string `mapstructure:"PORT"`
+	DBConn string `mapstructure:"DB_CONN"`
+}
+
+config := Config{
+		Port:   viper.GetString("PORT"),
+		DBConn: viper.GetString("DB_CONN"),
+}
+
+// Setup database
+	db, err := database.InitDB(config.DBConn)
+	if err != nil {
+		log.Fatal("Failed to initialize database:", err)
+	}
+	defer db.Close()
+
+// Setup routes
+http.HandleFunc("/api/produk", productHandler.HandleProducts)
+http.HandleFunc("/api/produk/", productHandler.HandleProductByID)
